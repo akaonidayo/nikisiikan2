d3cb193e217c49ee70788c430c1d7ad93e061f77
KeipamuKaihatu <Keipamu.Akaoni1@gmail.com>
2026-06-20 06:10:36 +0900

    ニキシー管時計のソースコードを追加

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/ikisiikan2/', // GitHub Pages用のベースパスを追加
+    base: '/nikisiikan2/', // GitHub Pages用のベースパスを追加
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
